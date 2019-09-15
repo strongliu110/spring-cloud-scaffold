f962cc42050189b043c11e2a1b915c56a07e5d6d
刘育哲 <liuyuzhe@liuyuzhedeMBP.lan>
2019-09-15 23:25:42 +0800

    first commit

---
+++ src/main/java/com/strongliu/springcloudscaffold/SpringCloudScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.strongliu.springcloudscaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudScaffoldApplication.class, args);
+	}
+
+}
